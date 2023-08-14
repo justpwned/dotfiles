3deab99cfe4773cfd2d7f0e61a5ca5cff2388976
justpwned <justpwnedfrom@gmail.com>
2023-08-14 15:03:44 +0500

    telescope searches now include dot files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,4 @@
-function vim.project_files()
-    local builtin = require('telescope.builtin')
-    -- We cache the results of "git rev-parse"
-    -- Process creation is expensive in Windows, so this reduces latency
-    local is_inside_work_tree = {}
-
-    local project_files = function()
-        local opts = {} -- define here if you want to define something
-
-        local cwd = vim.fn.getcwd()
-        if is_inside_work_tree[cwd] == nil then
-            vim.fn.system("git rev-parse --is-inside-work-tree")
-            is_inside_work_tree[cwd] = vim.v.shell_error == 0
-        end
-
-        if is_inside_work_tree[cwd] then
-            builtin.git_files(opts)
-        else
-            builtin.find_files(opts)
-        end
-    end
-
-    return project_files()
-end
-
-function vim.find_files_from_project_git_root()
+local function find_files_from_project_git_root()
     local function is_git_repo()
         vim.fn.system("git rev-parse --is-inside-work-tree")
         return vim.v.shell_error == 0
@@ -41,6 +16,29 @@ function vim.find_files_from_project_git_root()
     require("telescope.builtin").find_files(opts)
 end
 
+local function live_grep_from_project_git_root()
+    local function is_git_repo()
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+
+        return vim.v.shell_error == 0
+    end
+
+    local function get_git_root()
+        local dot_git_path = vim.fn.finddir(".git", ".;")
+        return vim.fn.fnamemodify(dot_git_path, ":h")
+    end
+
+    local opts = {}
+
+    if is_git_repo() then
+        opts = {
+            cwd = get_git_root(),
+        }
+    end
+
+    require("telescope.builtin").live_grep(opts)
+end
+
 return {
     'nvim-telescope/telescope.nvim',
     version = '*',
@@ -56,20 +54,26 @@ return {
     },
     config = function()
         local telescope = require('telescope')
+        local telescopeConfig = require('telescope.config')
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        table.insert(vimgrep_arguments, "--hidden")
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
+
         telescope.setup {
             defaults = {
-                layout_config = {
-                },
+                vimgrep_arguments = vimgrep_arguments,
                 mappings = {
                     i = {
                         ["<C-u>"] = false
-                    }
-                }
+                    },
+                },
             },
             pickers = {
                 find_files = {
                     theme = "dropdown",
                     previewer = false,
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
                 git_files = {
                     theme = "dropdown",
@@ -82,9 +86,9 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set("n", "<leader>ff", builtin.find_files)
-        vim.keymap.set("n", "<leader>fp", vim.find_files_from_project_git_root)
+        vim.keymap.set("n", "<leader>fp", find_files_from_project_git_root)
         vim.keymap.set("n", "<leader>fP", builtin.git_files)
-        vim.keymap.set("n", "<leader>fs", builtin.live_grep)
+        vim.keymap.set("n", "<leader>fs", live_grep_from_project_git_root)
         vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
     end,
 }
