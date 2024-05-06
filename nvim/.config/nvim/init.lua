e43b11a0fad1e92ee71121517750aac5ab6e8f7b
justpwned <justpwnedfrom@gmail.com>
2024-05-06 19:32:00 +0500

    update config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,21 +1,25 @@
-require("options")
-require("keymap")
+if vim.g.vscode then
+else
+	require("options")
+	require("keymap")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
+
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable",
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	require("lazy").setup("plugins", {
+		change_detection = {
+			notify = false,
+		},
 	})
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-	change_detection = {
-		notify = false,
-	},
-})
